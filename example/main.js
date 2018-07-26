95996cb7a9fa8ea5e5dadb82eb7d1a4e66231b6c
TidamCo <tidam7@gmail.com>
2018-07-26 17:20:43 +0200

    Pret a etre utilise

--- example/main.js
+++ example/main.js
@@ -1,72 +1,107 @@
-// @noflow
 
-import Expo from 'expo';
 import React, { Component, PureComponent } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import MasonryList from '@appandflow/masonry-list';
 
 const COLORS = ['green', 'blue', 'red'];
-const DATA = Array.from({ length: 50000 }).map((_, i) => ({
-  id: `item_${i}`,
-  height: Math.round(Math.random() * 100 + 50),
-  color: COLORS[i % COLORS.length],
-}));
 
 class Cell extends PureComponent {
-  componentDidMount() {
-    console.warn('mount cell');
-  }
+    componentDidMount() {
+        console.log('mount cell');
+    }
 
-  componentWillUnmount() {
-    console.warn('unmount cell');
-  }
+    componentWillUnmount() {
+        console.log('unmount cell');
+    }
 
-  render() {
-    const { item } = this.props;
-    return (
-      <View
-        style={[
-          styles.cell,
-          { height: item.height, backgroundColor: item.color },
-        ]}
-      >
-        <Text>{item.id}</Text>
-      </View>
-    );
-  }
+    render() {
+        const { item } = this.props;
+        return (
+            <View
+                style={[
+                    styles.cell,
+                    { height: item.height, backgroundColor: item.color },
+                ]}
+            >
+                <Text>{item.id}</Text>
+            </View>
+        );
+    }
 }
 
 class App extends Component {
-  state = { isRefreshing: false };
+    constructor(props) {
+        super(props);
+        this.state = { isRefreshing: false, data: [] };
+        setTimeout(this._addData, 1000);
+    }
 
-  _refreshRequest = () => {
-    this.setState({ isRefreshing: true });
-    setTimeout(() => {
-      this.setState({ isRefreshing: false });
-    }, 1000);
-  };
+    _addData = (reset = false) => {
+        const initialIndex = !!reset ? 0 : this.state.data.length;
+        const newData = Array.from({ length: 20 }).map((_, i) => ({
+          id: `item_${i + initialIndex}`,
+          height: Math.round(Math.random() * 100 + 50),
+          color: COLORS[(i + initialIndex) % COLORS.length],
+        }));
+        this.setState({
+            data: !!reset ? newData : this.state.data.concat(newData),
+            isRefreshing: false,
+        });
+    }
 
-  render() {
-    return (
-      <MasonryList
-        onRefresh={this._refreshRequest}
-        refreshing={this.state.isRefreshing}
-        data={DATA}
-        renderItem={({ item }) => <Cell item={item} />}
-        getHeightForItem={({ item }) => item.height + 2}
-        numColumns={2}
-        keyExtractor={item => item.id}
-      />
-    );
-  }
+    _refreshRequest = () => {
+        this.setState({ isRefreshing: true });
+        setTimeout(() => this._addData(true), 1000);
+    }
+
+    _renderHeader = () => {
+        return (
+            <View style={{ flex:1, height: 100, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Mon header</Text>
+            </View>
+        );
+    }
+
+    _renderFooter = () => {
+        return (
+            <View style={{ flex:1, height: 100, alignItems: 'center', justifyContent: 'center' }}>
+                <ActivityIndicator />
+            </View>
+        );
+    }
+
+    _onEndReached = () => {
+        setTimeout(this._addData, 1000);
+    }
+
+    render() {
+        return (
+            <MasonryList
+                onRefresh={this._refreshRequest}
+                refreshing={this.state.isRefreshing}
+                data={this.state.data}
+                renderItem={({ item }) => <Cell item={item} />}
+                getHeightForItem={({ item }) => item.height + 11}
+                numColumns={2}
+                keyExtractor={item => item.id}
+                ListHeaderComponent={this._renderHeader}
+                ListFooterComponent={this._renderFooter}
+                onEndReached={this._onEndReached}
+                onEndReachedThreshold={0.2}
+                contentContainerStyle={{ paddingHorizontal: 10 }}
+                verticalSeparatorWidth={10}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  cell: {
-    margin: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    cell: {
+        margin: 1,
+        marginTop: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
-Expo.registerRootComponent(App);
+export default App;
